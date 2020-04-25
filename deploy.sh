a3f57ddf7965a5e77f3e10e6524260f77bc4fb65
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2020-04-24 22:31:50 -0700

    Deploy scripts almost there.

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-npm install &&
-rsync --delete --force -avvP --exclude='git' ~/projects/dahlinghaustld/webroot/ dahlinghaus.net:/webroot &&
-ssh dahlinghaus.net 'sudo systemctl restart sails' &&
-echo "Sails deployed"
-
-
